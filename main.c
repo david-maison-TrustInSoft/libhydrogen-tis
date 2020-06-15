1d5c7f0eb51af7fcc96dccd46d2b2a8fde2e2337
Mike <tankf33der@disroot.org>
2020-06-15 20:14:23 +0300

    .

--- main.c
+++ main.c
@@ -9,9 +9,6 @@ typedef uint8_t u8;
     char name[size]; \
     for(size_t i = 0; i < size; i++) name[i] = i;
 
-// int hydro_hash_hash(uint8_t *out, size_t out_len, const void *in_, size_t in_len,
-                    // const char    ctx[hydro_hash_CONTEXTBYTES],
-                    // const uint8_t key[hydro_hash_KEYBYTES]);
 void hash(void) {
     ARRAY(out, 32);
     ARRAY(in , 32);
@@ -20,6 +17,44 @@ void hash(void) {
     hydro_hash_hash(out, 32, in, 32, ctx, key);
 }
 
+void kdf(void) {
+    ARRAY(out, 32);
+      CTX(ctx,  8);
+    ARRAY(key, 32);
+    hydro_kdf_derive_from_key(out, 32, 777, ctx, key);
+}
+
+// #define hydro_secretbox_CONTEXTBYTES 8
+// #define hydro_secretbox_HEADERBYTES (20 + 16)
+// #define hydro_secretbox_KEYBYTES 32
+// #define hydro_secretbox_PROBEBYTES 16
+//
+// void hydro_secretbox_keygen(uint8_t key[hydro_secretbox_KEYBYTES]);
+//
+// int hydro_secretbox_encrypt(uint8_t *c, const void *m_, size_t mlen, uint64_t msg_id,
+                            // const char    ctx[hydro_secretbox_CONTEXTBYTES],
+                            // const uint8_t key[hydro_secretbox_KEYBYTES]);
+//
+// int hydro_secretbox_decrypt(void *m_, const uint8_t *c, size_t clen, uint64_t msg_id,
+                            // const char    ctx[hydro_secretbox_CONTEXTBYTES],
+                            // const uint8_t key[hydro_secretbox_KEYBYTES])
+    // _hydro_attr_warn_unused_result_;
+
+void secret(void) {
+    ARRAY(out, 68); // out = 36 + 32
+    ARRAY(  m, 32);
+      CTX(ctx, 8);
+    ARRAY(key, 32);
+    int i = 0;
+
+    i = hydro_secretbox_encrypt(out, m, 32, 777, ctx, key);
+    i = hydro_secretbox_decrypt(m, out, 68, 777, ctx, key);
+    i++;
+}
+
 int main(void) {
+    hash();
+    kdf();
+    secret();
     return 0;
 }
