8ce2ea3f87ef142bb308f8ae42a6fe0424421755
Mike <tankf33der@disroot.org>
2020-06-15 20:21:20 +0300

    .

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "hydrogen.h"
 
 typedef uint8_t u8;
@@ -24,37 +25,25 @@ void kdf(void) {
     hydro_kdf_derive_from_key(out, 32, 777, ctx, key);
 }
 
-// #define hydro_secretbox_CONTEXTBYTES 8
-// #define hydro_secretbox_HEADERBYTES (20 + 16)
-// #define hydro_secretbox_KEYBYTES 32
-// #define hydro_secretbox_PROBEBYTES 16
-//
-// void hydro_secretbox_keygen(uint8_t key[hydro_secretbox_KEYBYTES]);
-//
-// int hydro_secretbox_encrypt(uint8_t *c, const void *m_, size_t mlen, uint64_t msg_id,
-                            // const char    ctx[hydro_secretbox_CONTEXTBYTES],
-                            // const uint8_t key[hydro_secretbox_KEYBYTES]);
-//
-// int hydro_secretbox_decrypt(void *m_, const uint8_t *c, size_t clen, uint64_t msg_id,
-                            // const char    ctx[hydro_secretbox_CONTEXTBYTES],
-                            // const uint8_t key[hydro_secretbox_KEYBYTES])
-    // _hydro_attr_warn_unused_result_;
-
-void secret(void) {
+void secretbox(void) {
     ARRAY(out, 68); // out = 36 + 32
     ARRAY(  m, 32);
       CTX(ctx, 8);
     ARRAY(key, 32);
+    ARRAY(prb, 16);
     int i = 0;
 
-    i = hydro_secretbox_encrypt(out, m, 32, 777, ctx, key);
-    i = hydro_secretbox_decrypt(m, out, 68, 777, ctx, key);
+    i = hydro_secretbox_encrypt (out,   m, 32, 777, ctx, key);
+        hydro_secretbox_probe_create(prb, out, 68, ctx, key);
+    i = hydro_secretbox_probe_verify(prb, out, 68, ctx, key);
+    assert(i == 0);
+    i = hydro_secretbox_decrypt (  m, out, 68, 777, ctx, key);
     i++;
 }
 
 int main(void) {
     hash();
     kdf();
-    secret();
+    secretbox();
     return 0;
 }
